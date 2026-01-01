b3df7a793c7eb89dcb58755b82031c1c73b1071d
Artur Neumann <artur@jankaritech.com>
2026-01-01 14:06:38 +0545

    convert note to png and store on drive

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import joplin from 'api';
 import fs = require('fs');
 import {SettingItemSubType, SettingItemType} from "../api/types";
-import * as path from "node:path";
+import * as imagejs from "image-js"
+import {SupernoteX, toImage} from "supernote-typescript";
+import path = require('path');
 
 const registerSettings = async () => {
 	const sectionName = 'supernote';
@@ -23,6 +25,17 @@ const registerSettings = async () => {
 	});
 };
 
+function readFileToUint8Array(filePath: string): Promise<Uint8Array> {
+	return new Promise((resolve, reject) => {
+		fs.readFile(filePath, (err, data) => {
+			if (err) {
+				reject(err);
+			} else {
+				resolve(new Uint8Array(data.buffer));
+			}
+		});
+	});
+}
 
 joplin.plugins.register({
 
@@ -41,5 +54,32 @@ joplin.plugins.register({
 
 		console.info(`found ${noteFiles.length} files`);
 		console.info(noteFiles);
+
+		for (const noteFile of noteFiles) {
+			const fullPath = path.join(supernoteNotesDirectory, noteFile);
+			console.info(`Reading note: ${fullPath}`);
+			let sn = new SupernoteX(await readFileToUint8Array(fullPath));
+			console.info(`In ${noteFile} there are ${sn.pages.length} pages!`);
+
+			let images = await toImage(sn)
+			console.info(images.length);
+			const fullOutputPath = path.join(supernoteNotesDirectory, 'output', `${noteFile}-0.png`);
+
+			for await (const [index, image] of images.entries()) {
+				const fullOutputPath = path.join(supernoteNotesDirectory, 'output', `${noteFile}-${index}.png`)
+
+				const outputDirName = path.dirname(fullOutputPath);
+				if (!fs.existsSync(outputDirName)) {
+					fs.mkdirSync(outputDirName, { recursive: true });
+				}
+
+				try {
+					imagejs.writeSync(path.join(fullOutputPath,) , image)
+				} catch (e) {
+					console.error(e)
+				}
+
+			}
+		}
 	},
 });

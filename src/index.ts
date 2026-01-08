386b0807993ab8dd6b8ddb2a86ad08e4ab3b3019
Artur Neumann <artur@jankaritech.com>
2026-01-08 16:32:30 +0545

    sync periodically
    fixes #4

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,8 @@ import path = require('path');
 
 import os = require('os');
 
+let syncInterval: NodeJS.Timeout;
+
 const registerSettings = async () => {
     const sectionName = 'supernote';
     await joplin.settings.registerSection(sectionName, {
@@ -45,68 +47,89 @@ const registerSettings = async () => {
             description: 'External link of the Joplin Notebook, to which you want to sync the .note files to.',
         },
     });
+
+    await joplin.settings.registerSettings({
+        'sync-interval-seconds': {
+            value: 600,
+            type: SettingItemType.Int,
+            section: sectionName,
+            public: true,
+            label: 'Sync Interval in seconds',
+            description: 'How frequent to sync the .note files to Joplin.',
+        },
+    });
 };
 
-joplin.plugins.register({
+const run = async () => {
+    const supernoteNotesDirectory = await joplin.settings.value('supernote-notes-directory');
+    const destinationNotebookExternalLink = await joplin.settings.value('destination-notebook');
+    const destinationRootNotebookId = await getDestinationRootNotebook(destinationNotebookExternalLink);
 
+    // eslint-disable-next-line no-console
+    console.info('Supernote plugin started!');
+    console.info('Notes are stored in: ' + supernoteNotesDirectory);
+    if (!fs.existsSync(supernoteNotesDirectory)) {
+        const message = 'The supernote directory does not exist!'
+        await showMessage(
+            ToastType.Error,
+            message
+        );
+        throw Error(message);
+    }
+
+    const files = await fs.promises.readdir(supernoteNotesDirectory, {recursive: true});
+    const noteFiles = files.filter(file => file.endsWith('.note'));
+
+    console.info(`found ${noteFiles.length} files`);
+    console.info(noteFiles);
+
+    const tmpFolder = fs.mkdtempSync(path.join(os.tmpdir(), 'joplin-supernote-sync'));
+    for (const noteFile of noteFiles) {
+        const destinationNotebookId = await createJoplinNotebookStructure(noteFile, destinationRootNotebookId);
+        const fullPathOfNoteFile = path.join(supernoteNotesDirectory, noteFile)
+        const statsNoteFile = fs.statSync(fullPathOfNoteFile);
+        const matchingNote = await findMatchingNote(destinationNotebookId, noteFile);
+
+        if (matchingNote && statsNoteFile.mtime.getTime() < matchingNote.updated_time) {
+            console.info(`skipping ${noteFile} as file mtime is ${statsNoteFile.mtime.getTime()} and note updated time: ${matchingNote.updated_time} `);
+            continue
+        }
+
+        let toastMessage = `syncing file '${fullPathOfNoteFile}'`
+        if (matchingNote) {
+            toastMessage += ` - to note '${matchingNote.title}'`;
+        }
+        await showMessage(ToastType.Info, toastMessage);
+        const sn = new SupernoteX(await readFileToUint8Array(fullPathOfNoteFile));
+        let noteContent = "";
+        for (const page of sn.pages) {
+            if (page.paragraphs.trim().length > 0) {
+                noteContent += page.paragraphs + "\n\n";
+            }
+        }
+        for (const resource of await createResources(sn, tmpFolder, noteFile)) {
+            noteContent += `![${resource.title}](:/${resource.id})\n`;
+        }
+        await writeNote(destinationNotebookId, matchingNote, noteFile, noteContent)
+    }
+    try {
+        fs.rmSync(tmpFolder, {recursive: true, force: true});
+    } catch (e) {
+        console.error(e);
+    }
+};
+
+
+joplin.plugins.register({
     onStart: async function () {
         await registerSettings();
-        const dialogs = joplin.views.dialogs;
-        const supernoteNotesDirectory = await joplin.settings.value('supernote-notes-directory');
-        const destinationNotebookExternalLink = await joplin.settings.value('destination-notebook');
-        const destinationRootNotebookId = await getDestinationRootNotebook(destinationNotebookExternalLink);
-
-        // eslint-disable-next-line no-console
-        console.info('Supernote plugin started!');
-        console.info('Notes are stored in: ' + supernoteNotesDirectory);
-        if (!fs.existsSync(supernoteNotesDirectory)) {
-            const message = 'The supernote directory does not exist!'
-            await showMessage(
-                ToastType.Error,
-                message
-            );
-            throw Error(message);
-        }
-
-        const files = await fs.promises.readdir(supernoteNotesDirectory, {recursive: true});
-        const noteFiles = files.filter(file => file.endsWith('.note'));
-
-        console.info(`found ${noteFiles.length} files`);
-        console.info(noteFiles);
-
-        const tmpFolder = fs.mkdtempSync(path.join(os.tmpdir(), 'joplin-supernote-sync'));
-        for (const noteFile of noteFiles) {
-            const destinationNotebookId = await createJoplinNotebookStructure(noteFile, destinationRootNotebookId);
-            const fullPathOfNoteFile = path.join(supernoteNotesDirectory, noteFile)
-            const statsNoteFile = fs.statSync(fullPathOfNoteFile);
-            const matchingNote = await findMatchingNote(destinationNotebookId, noteFile);
-
-            if (matchingNote && statsNoteFile.mtime.getTime() < matchingNote.updated_time) {
-                console.info(`skipping ${noteFile} as file mtime is ${statsNoteFile.mtime.getTime()} and note updated time: ${matchingNote.updated_time} `);
-                continue
-            }
-
-            let toastMessage = `syncing file '${fullPathOfNoteFile}'`
-            if (matchingNote) {
-                toastMessage += ` - to note '${matchingNote.title}'`;
-            }
-            await showMessage(ToastType.Info, toastMessage );
-            const sn = new SupernoteX(await readFileToUint8Array(fullPathOfNoteFile));
-            let noteContent = "";
-            for (const page of sn.pages) {
-                if (page.paragraphs.trim().length > 0) {
-                    noteContent += page.paragraphs + "\n\n";
-                }
-            }
-            for (const resource of await createResources(sn, tmpFolder, noteFile)) {
-                noteContent += `![${resource.title}](:/${resource.id})\n`;
-            }
-            await writeNote(destinationNotebookId, matchingNote, noteFile, noteContent)
-        }
-        try {
-            fs.rmSync(tmpFolder, {recursive: true, force: true});
-        } catch (e) {
-            console.error(e);
-        }
-    },
+        let interval = await joplin.settings.value('sync-interval-seconds') * 1000;
+        await run();
+        syncInterval = setInterval(run, interval);
+        await joplin.settings.onChange(async (event) => {
+            clearInterval(syncInterval);
+            interval = await joplin.settings.value('sync-interval-seconds') * 1000;
+            syncInterval = setInterval(run, interval);
+        });
+    }
 });
